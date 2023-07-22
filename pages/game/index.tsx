dd1d4548c5e834245a22cf05a890c3ec42c2879e
Leonardo-Gerling <leonardodag123456@gmail.com>
2023-07-22 12:43:12 +0200

    Nichts wichtiges

--- pages/game/index.tsx
+++ pages/game/index.tsx
@@ -5,18 +5,18 @@ export default function Game(): ReactElement {
     const [apples, setApples] = useState(0)
 
     return (
-        <>
+        <div className="grid grid-cols-3">
             <div aria-label="shop" className="select-none grid auto-cols-fr divide divide-solid m-0 max-w-[20vh]">
-                <div>div</div>
-                <div>div</div>
-                <div>div</div>
-                <div>div</div>
-                <div>div</div>
-                <div>div</div>
-                <div>div</div>
-                <div>div</div>
-                <div>div</div>
-                <div>div</div>
+                <div>Shop</div>
+                <div>Shop</div>
+                <div>Shop</div>
+                <div>Shop</div>
+                <div>Shop</div>
+                <div>Shop</div>
+                <div>Shop</div>
+                <div>Shop</div>
+                <div>Shop</div>
+                <div>Shop</div>
             </div>
 
             <div aria-label="mid-section" className="select-none grid grid-cols-2 mx-auto w-[70rem] p-40 shadow-md rounded-b-xl">
@@ -26,13 +26,20 @@ export default function Game(): ReactElement {
                     <p className="text-white bg-[#76b02a] mt-10 text-center text-4xl py-2">{apples} Apples</p>
                 </div>
                 <div className="grid grid-rows-5 divide-y divide-dotted ml-6">
-                    <div>div</div>
-                    <div>div</div>
-                    <div>div</div>
-                    <div>div</div>
-                    <div>div</div>
+                <div>Mid</div>
+                    <div>Mid</div>
+                    <div>Mid</div>
+                    <div>Mid</div>
+                    <div>Mid</div>
                 </div>
             </div>
-        </>
+
+            <div aria-label="left-section" className="grid grid-rows-4">
+                <div>Left</div>
+                <div>Left</div>
+                <div>Left</div>
+                <div>Left</div>
+            </div>
+        </div>
     )
 }
