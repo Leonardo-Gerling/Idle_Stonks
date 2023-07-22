b90131ba37a03e8e29345dcd9f376e709afde52e
Leonardo-Gerling <leonardodag123456@gmail.com>
2023-07-22 12:36:56 +0200

    Initial commit

---
+++ pages/game/index.tsx
@@ -0,0 +1,38 @@
+import { ReactElement, useState } from "react";
+import apple_tree from "../../public/apple_tree.png";
+
+export default function Game(): ReactElement {
+    const [apples, setApples] = useState(0)
+
+    return (
+        <>
+            <div aria-label="shop" className="select-none grid auto-cols-fr divide divide-solid m-0 max-w-[20vh]">
+                <div>div</div>
+                <div>div</div>
+                <div>div</div>
+                <div>div</div>
+                <div>div</div>
+                <div>div</div>
+                <div>div</div>
+                <div>div</div>
+                <div>div</div>
+                <div>div</div>
+            </div>
+
+            <div aria-label="mid-section" className="select-none grid grid-cols-2 mx-auto w-[70rem] p-40 shadow-md rounded-b-xl">
+                <div className="mr-6">
+                    <img className="h-96 w-96 cursor-pointer" onClick={() => setApples(apples + 1)} src={apple_tree.src}/>
+                    
+                    <p className="text-white bg-[#76b02a] mt-10 text-center text-4xl py-2">{apples} Apples</p>
+                </div>
+                <div className="grid grid-rows-5 divide-y divide-dotted ml-6">
+                    <div>div</div>
+                    <div>div</div>
+                    <div>div</div>
+                    <div>div</div>
+                    <div>div</div>
+                </div>
+            </div>
+        </>
+    )
+}
