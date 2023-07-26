628ab45bbc80a71b20d92b31c7a83f968ff68c65
Leonardo-Gerling <leonardodag123456@gmail.com>
2023-07-26 13:51:10 +0200

    More items

--- components/game/shop_tile.tsx
+++ components/game/shop_tile.tsx
@@ -4,14 +4,15 @@ interface ShopTileProps {
     className?: string,
     itemName: string
     initialPrice: number
+    setPriceMethod: Function
 }
 
-export default function ShopTile({ className, itemName, initialPrice }: ShopTileProps): ReactElement {
+export default function ShopTile({ className, itemName, initialPrice, setPriceMethod }: ShopTileProps): ReactElement {
     const [count, setCount] = useState(0);
     const [price, setPrice] = useState(initialPrice);
 
     return (
-        <button className="h-24" onClick={() => { setCount(count + 1); setPrice(price + 5) }}>
+        <button className="h-24" onClick={() => { setCount(count + 1); setPrice(setPriceMethod(price)) }}>
             {itemName} costs {price}. You currently have {count}.
         </button>
     )
